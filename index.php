76f746adbba9e105fff2e18bd745fac84715c33a
kutsz <valik.kuts@gmail.com>
2016-11-27 19:34:18 +0200

    third commit

--- index.php
+++ index.php
@@ -5,13 +5,5 @@
 
 	$region = getRegion();
 
-	// $email = 'hello@www';
-	// $person = getPersonData($email);
-	// $namePerson = $person['name'];
-	// $emailPerson = $person['email'];
-	//  $territoryPerson = $person['territory'];
-	// list($regionPerson,$cityPerson,$areaPerson) = explode(",", $territoryPerson);
-
-
 	require 'view.php';
 
