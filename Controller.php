0c3aaec156b7efa6229b69962150071e4cd7c88a
kutsz <valik.kuts@gmail.com>
2016-11-30 14:23:22 +0200

    Add files via upload

---
+++ Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+* 
+*/
+
+require_once 'Model.php'; 
+
+class Controller 
+{
+	public static function view()
+	{
+		 Model::db_connection(); // open database connection
+
+		$region = Model::getRegion();
+
+		require_once 'view.php';
+
+		 Model::close_db_connection(); // close database connection
+	}
+
+	 
+}
+
